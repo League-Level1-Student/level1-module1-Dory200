3e7ea75537fadda1a9d42b5a55b15aa14d966476
tailw <tailw@DESKTOP-MR3PNPI>
2021-02-16 16:58:03 -0800

    half way through race_car

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -1,4 +1,7 @@
 package _01_race_car;
+
+import javax.swing.JOptionPane;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2019
  *    Level 1
@@ -11,13 +14,15 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+		RaceCar race = new RaceCar("BMW", 5);
 		// 2. Print the RaceCar's position in the race
-
+		System.out.println(race.getPositionInRace());
 		// 3. Crash the RaceCar
-		
+		race.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+		if(race.isDamaged()) {
+			
+		}
 		// 5. Help the car move into first place.
 
 	}
